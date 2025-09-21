f9c90756ae02b2b9884f5c8ddf0b7e85f79c722e
skyyygal <heeramnakp@gmail.com>
2025-09-22 03:55:35 +0530

    Initial

---
+++ lib/camera_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Camerascreen extends StatelessWidget {
+  const Camerascreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
