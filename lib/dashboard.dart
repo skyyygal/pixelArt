32b0a126c1ae799c7f93e86157513d05dc9881cf
skyyygal <heeramnakp@gmail.com>
2025-10-27 22:27:16 +0530

    Rive Integration

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:pixel_art/camera_screen.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Dashboard')),
+      body: Column(
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Camerascreen()),
+                );
+              },
+              child: Text("Start"),
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              'assets/images/character.png',
+              height: 300, // taille du personnage
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:pixel_art/dashboard.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await RiveFile.initialize();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Dashboard(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FireRive(),
     );
   }
 }
+
+class FireRive extends StatefulWidget {
+  const FireRive({super.key});
+
+  @override
+  State<FireRive> createState() => _FireRiveState();
+}
+
+class _FireRiveState extends State<FireRive> {
+  StateMachineController? _stateMachineController;
+  Artboard? mainArtBoard;
+  SMIBool? check;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRiveFile();
+  }
+
+  Future<void> _loadRiveFile() async {
+    final data = await rootBundle.load('assets/images/fire.riv');
+    final file = RiveFile.import(data);
+    final artboard = file.mainArtboard;
+    _stateMachineController = StateMachineController.fromArtboard(
+      artboard,
+      'State Machine 1',
+    );
+
+    // if (artboard.animations.isNotEmpty) {
+    //   final firstAnim = artboard.animations.first.name;
+    //   artboard.addController(SimpleAnimation(firstAnim));
+    //   debugPrint('▶️ Playing animation: $firstAnim');
+    // }
+    if (_stateMachineController != null) {
+      artboard.addController(_stateMachineController!);
+    }
+    mainArtBoard = artboard;
+
+    check = _stateMachineController!.findSMI('ON');
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Rive Fire Animation')),
+      body: mainArtBoard != null
+          ? InkWell(
+              onTap: () {
+                check!.value = !check!.value;
+                // if (check!.value) {
+                //   check!.value = false;
+                // } else {
+                //   check!.value = true;
+                // }
+              },
+              child: SizedBox(
+                height: double.infinity,
+                width: double.infinity,
+                child: Rive(artboard: mainArtBoard!, fit: BoxFit.cover),
+              ),
+            )
+          : Container(),
+    );
+  }
+}
+/* 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await RiveFile.initialize();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FireRive(),
+    );
+  }
+}
+
+class FireRive extends StatefulWidget {
+  const FireRive({super.key});
+
+  @override
+  State<FireRive> createState() => _FireRiveState();
+}
+
+class _FireRiveState extends State<FireRive> {
+  StateMachineController? _stateMachineController;
+  Artboard? mainArtBoard;
+  SMIBool? check;
+  @override
+  void initState() {
+    super.initState();
+
+    rootBundle.load("assets/images/fire.riv").then((riveByteData) {
+      var riveFile = RiveFile.import(riveByteData);
+
+      var artboard = riveFile.mainArtboard;
+      _stateMachineController = StateMachineController.fromArtboard(
+        artboard,
+        'State Machine 1',
+      );
+      if (_stateMachineController != null) {
+        artboard.addController(_stateMachineController!);
+
+        check = _stateMachineController!.findSMI('ON');
+        setState(() {
+          mainArtBoard = artboard;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Rive Fire Animation')),
+      body: mainArtBoard != null ? Rive(artboard: mainArtBoard!) : Container(),
+    );
+  }
+}
+ */
